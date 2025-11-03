7b99cf9e64d5bf3c059ddae4f223d0e9a7a8745e
shivaCode-2 <141118860+shivaCode-2@users.noreply.github.com>
2025-11-03 21:56:33 +0530

    Update runlabview.sh

--- runlabview.sh
+++ runlabview.sh
@@ -15,17 +15,15 @@ fi
 echo "Running LabVIEWCLI MassCompile with following parameters:"
 echo "DirectorytoCompile: $MASSCOMPILE_DIR"
 
-OUTPUT_MASSCOMPILE=$(LabVIEWCLI -LogToConsole TRUE \
+LabVIEWCLI -LogToConsole TRUE \
 -OperationName MassCompile \
 -DirectoryToCompile $MASSCOMPILE_DIR \
--LabVIEWPath $LABVIEW_PATH)
+-LabVIEWPath $LABVIEW_PATH
 
 echo " "
 echo "Done Running Masscompile Operation"
 echo "Printing Results..."
 echo " "
-echo "########################################################################################"
-echo $OUTPUT_MASSCOMPILE
 echo "########################################################################################"
 echo " "
 
@@ -35,11 +33,11 @@ echo "ReportPath: $REPORT_PATH"
 echo " "
 
 # Run the LabVIEWCLI VIA command.
-OUTPUT=$(LabVIEWCLI -LogToConsole TRUE \
+LabVIEWCLI -LogToConsole TRUE \
 -OperationName RunVIAnalyzer \
 -ConfigPath $CONFIG_FILE \
 -ReportPath $REPORT_PATH \
--LabVIEWPath $LABVIEW_PATH)
+-LabVIEWPath $LABVIEW_PATH
 
 echo "Done running of VI Analyzer Tests"
 echo "Printing Results..."
